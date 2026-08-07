8723d234779b1669655688eaec0768093c1fc378
Abraham Hernandez <habraham0413@gmail.com>
2026-08-06 18:20:46 -0600

    Fase4 Completada, persistencia de datos y estilizado de botones editar/eliminar

--- app-notas/script.js
+++ app-notas/script.js
@@ -1,5 +1,12 @@
 let notes = [];
 
+const loadNotes = () => {
+  const noteSavedString = localStorage.getItem("myNotes");
+  if (noteSavedString !== null) {
+    notes = JSON.parse(noteSavedString);
+  }
+};
+
 const notesContainer = document.querySelector("#notes-container");
 
 const renderNotes = () => {
@@ -12,19 +19,32 @@ const renderNotes = () => {
     const noteText = document.createElement("p");
     noteText.textContent = note.text;
 
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("btn-container");
+
     const noteEdit = document.createElement("button");
+    noteEdit.classList.add("btn", "btn-edit");
     noteEdit.type = "button";
     noteEdit.textContent = "Editar Nota";
+
     const noteDelete = document.createElement("button");
+    noteDelete.classList.add("btn", "btn-delete");
     noteDelete.type = "button";
     noteDelete.textContent = "Eliminar Nota";
 
     noteCard.appendChild(noteText);
-    noteCard.appendChild(noteEdit);
-    noteCard.appendChild(noteDelete);
+    noteCard.appendChild(btnContainer);
+    btnContainer.appendChild(noteEdit);
+    btnContainer.appendChild(noteDelete);
 
     notesContainer.appendChild(noteCard);
   }
 };
 
+const saveNotes = () => {
+  const notesToString = JSON.stringify(notes);
+  localStorage.setItem("myNotes", notesToString);
+};
+
+loadNotes();
 renderNotes();
